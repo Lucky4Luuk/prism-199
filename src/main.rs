27e4b885ac27e8ef0e0de3720377b738269beccc
Luuk van Oijen <lazyluuk.channel@gmail.com>
2022-07-17 04:36:08 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+const WINDOW_WIDTH: u32 = 1680;
+const WINDOW_HEIGHT: u32 = 720;
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("PRISM-199 - FANTASY COMPUTER - V[0.1]")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WINDOW_WIDTH / 10, WINDOW_HEIGHT / 10, surface_texture).expect("Failed to create Pixels object!")
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        if let Event::RedrawRequested(_) = event {
+            // draw(pixels.get_frame());
+            if pixels
+                .render()
+                .map_err(|e| eprintln!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+
+            // Update internal state and request a redraw
+            window.request_redraw();
+        }
+    });
+}
